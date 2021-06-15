8fc42653229f9c2970f9f4b5eb9518ebd3bd1f60
Nikuze Diop <nikuzediop@gmail.com>
2021-06-15 23:42:45 +0200

    spinner loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider, Flex, Spinner } from "@chakra-ui/react";
 import Data from "./components/Data";
 import Search from "./components/Search";
 import Map from "./components/Map";
@@ -10,6 +10,7 @@ function App() {
   const [details, setDetails] = useState([]);
   const [search, setSearch] = useState("");
   const [ip, setIP] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     setSearch(e.target.value);
@@ -27,6 +28,7 @@ function App() {
       )
       .then((response) => {
         setDetails([response.data]);
+        setLoading(true);
       })
       .catch((error) => console.log(error));
   }, [ip]);
@@ -34,8 +36,25 @@ function App() {
   return (
     <ChakraProvider>
       <Search onchange={handleChange} onsubmit={handleSubmit} />
-      <Data details={details} />
-      <Map locations={details} />
+      <Box>
+        {loading ? (
+          <Data details={details} />
+        ) : (
+          <Flex justifyContent="center">
+            <Spinner color="red.500" size="xl" speed=".8s" />
+          </Flex>
+        )}
+      </Box>
+
+      <Box>
+        {loading ? (
+          <Map locations={details} />
+        ) : (
+          <Flex justifyContent="center" alignItems="center" h="70vh">
+            <Spinner color="red.500" size="xl" speed=".8s" />
+          </Flex>
+        )}
+      </Box>
     </ChakraProvider>
   );
 }
